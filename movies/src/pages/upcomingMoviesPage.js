d522f1e213c977f97407edadf0fe9ae70c6fb05b
DisSys <20084833@mail.wit.ie>
2023-11-15 22:23:17 +0000

    Fix upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -1,18 +1,12 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from "../components/templateMovieListPage"
 import { getUpcoming } from "../api/tmdb-api"
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 
-const HomePage = (props) => {
+const UpcomingMoviesPage = (props) => {
     const [ movies, setMovies ] = useState([]);
     const favorites = movies.filter((m) => m.favorites);
     localStorage.setItem("favorites", JSON.stringify(favorites));
-
-    const addToFavorites = (movieId) => {
-        const updatedMovies = movies.map((m) => 
-            m.id === movieId ? { ...m, favorites: true } : m
-        );
-        setMovies(updatedMovies);
-    };
 
     useEffect(() => {
         getUpcoming().then(movies => {
@@ -25,9 +19,11 @@ const HomePage = (props) => {
         <PageTemplate
             title="Upcoming Movies"
             movies={movies}
-            selectFavorite={addToFavorites}
+            action={(movie) => {
+                return <AddToFavoritesIcon movie={movie} />
+            }}
         />
     );
 };
 
-export default HomePage;
+export default UpcomingMoviesPage;
